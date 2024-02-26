8b368cf3b30564b4c534389ae018955a408a9d94
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-02-25 22:56:58 -0300

    setting api, transactions context

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { TransactionsProvider } from '@/contexts/TransactionsContext.tsx'
+
 import { Transactions } from './pages/Transactions'
 
 export function App() {
     return (
-        <>
+        <TransactionsProvider>
             <Transactions />
-        </>
+        </TransactionsProvider>
     )
 }
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,13 @@
+import { useContext } from 'react'
+
+import { Header } from '@/components/Header'
+import { Summary } from '@/components/Summary'
+import { TransactionsContext } from '@/contexts/TransactionsContext.tsx'
 import { SearchForm } from '@/pages/Transactions/Components/SearchForm'
 
-import { Header } from '../../components/Header'
-import { Summary } from '../../components/Summary'
-
 export function Transactions() {
+    const { transactions } = useContext(TransactionsContext)
+
     return (
         <div>
             <Header />
@@ -12,20 +16,18 @@ export function Transactions() {
             <div className="mx-auto my-[4rem] w-full max-w-[min(70rem,100%-2rem)]">
                 <SearchForm />
 
-                <table className="mt-[1.5rem] w-full border-separate border-spacing-y-[0.5rem] border-spacing-y-[0]">
+                <table className="mt-[1.5rem] w-full border-separate border-spacing-y-[0.5rem]">
                     <tbody>
-                        <tr>
-                            <td className="rounded-l-[6px] bg-gray-700 px-[2rem] py-[1.5rem]" width="50%">Desenvolvimento de site</td>
-                            <td className="bg-gray-700 px-[2rem] py-[1.5rem] text-green-300">R$ 12.000,00</td>
-                            <td className="bg-gray-700 px-[2rem] py-[1.5rem]">Venda</td>
-                            <td className="rounded-r-[6px] bg-gray-700 px-[2rem] py-[1.5rem]">13/04/2022</td>
-                        </tr>
-                        <tr>
-                            <td className="rounded-l-[6px] bg-gray-700 px-[2rem] py-[1.5rem]" width="50%">Desenvolvimento de site</td>
-                            <td className="bg-gray-700 px-[2rem] py-[1.5rem] text-red-300">R$ 12.000,00</td>
-                            <td className="bg-gray-700 px-[2rem] py-[1.5rem]">Venda</td>
-                            <td className="rounded-r-[6px] bg-gray-700 px-[2rem] py-[1.5rem]">13/04/2022</td>
-                        </tr>
+                        {transactions.map(transaction => (
+                            <tr key={transaction.id}>
+                                <td className="rounded-l-[6px] bg-gray-700 px-[2rem] py-[1.5rem]" width="50%">
+                                    {transaction.description}
+                                </td>
+                                <td className={`bg-gray-700 px-[2rem] py-[1.5rem] ${transaction.type === 'income' ? 'text-green-300' : 'text-red-300' }`}>{transaction.price}</td>
+                                <td className="bg-gray-700 px-[2rem] py-[1.5rem]">{transaction.category}</td>
+                                <td className="rounded-r-[6px] bg-gray-700 px-[2rem] py-[1.5rem]">{transaction.createdAt}</td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
