ba7729e92deef2fadaac5ab72592ad9f3bf80387
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-02-27 23:36:12 -0300

    fisrt custom hook, hook form configuration

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,30 +1,13 @@
 import theme from '@tailwindConfig'
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
-import { useContext } from 'react'
 
-import { TransactionsContext } from '@/contexts/TransactionsContext.tsx'
+import { useSummary } from '@/hooks/useSummary.ts'
 import { priceFormatter } from '@/utils/formatter.ts'
 
 const colors = theme.theme.colors
 
 export function Summary() {
-    const { transactions } = useContext(TransactionsContext)
-
-    const summary = transactions.reduce((acc, transaction) => {
-        if(transaction.type === 'income') {
-            acc.income += transaction.price
-            acc.total += transaction.price
-        } else {
-            acc.outcome += transaction.price
-            acc.total -= transaction.price
-        }
-
-        return acc
-    }, {
-        income: 0,
-        outcome: 0,
-        total: 0
-    })
+    const summary = useSummary()
 
     return (
         <section className="mx-auto mt-[-5rem] grid w-full max-w-[min(70rem,100%-2rem)] grid-cols-3 gap-[2rem]">
--- src/pages/Transactions/Components/SearchForm/index.tsx
+++ src/pages/Transactions/Components/SearchForm/index.tsx
@@ -1,20 +1,41 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { MagnifyingGlass } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+
+const searchFormSchema = z.object({
+    query: z.string(),
+
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
-    return (
-        <form className="flex gap-[1rem]">
+    const { register, handleSubmit } = useForm<SearchFormInputs>({
+        resolver: zodResolver(searchFormSchema),
+    })
 
+    function handleSearchTransactions(data: SearchFormInputs) {
+        console.log(data)
+    }
+
+    return (
+        <form
+            className="flex gap-[1rem]"
+            onSubmit={handleSubmit(handleSearchTransactions)}
+        >
             <input
                 type="text"
                 placeholder="Busque por transações"
                 className="flex-1  rounded-[6px] border-0 bg-gray-900 p-[1rem] text-gray-300 placeholder:text-gray-500"
+                {...register('query')}
             />
             <button
                 type="submit"
                 className="flex items-center gap-[0.75rem] rounded-[6px] border-[1px] border-green-300 bg-transparent p-[1rem] font-bold text-green-300 transition hover:border-green-500 hover:bg-green-500 hover:text-white"
             >
                 <MagnifyingGlass />
-          Buscar
+                Buscar
             </button>
         </form>
     )
